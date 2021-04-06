c6f756644143a24735920ccfdc345c449e17b227
slavo3dev <slavo@slavo3.com>
2021-04-06 10:18:46 -0400

    removing npm package

--- frontend-evybe/components/layout/index.js
+++ frontend-evybe/components/layout/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Router from "next/router";
 import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 Router.onRouteChangeStart = (url) => NProgress.start();
 Router.onRouteChangeComplete = (url) => NProgress.done();
@@ -16,10 +17,6 @@ export const Layout = (props) => {
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossOrigin="anonymous"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
       />
     </Head>
   );
