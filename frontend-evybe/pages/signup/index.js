c9ecf1e1a46c3178bbd81721a0260aef77e7bd23
slavo3dev <slavo@slavo3.com>
2021-04-06 11:31:48 -0400

    update state

--- frontend-evybe/pages/signup/index.js
+++ frontend-evybe/pages/signup/index.js
@@ -1,5 +1,73 @@
 import { Layout } from "../../components";
+import { useState } from "react";
 
 export default function signUp() {
-  return <Layout>Sign up</Layout>;
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: "",
+    btnText: "Sign up",
+  });
+
+  const { name, email, password, error, success, btnText } = userInfo;
+
+  const handleChange = (inputValue) => (e) => {
+    setUserInfo({
+      ...userInfo,
+      [inputValue]: e.target.value,
+      error: "",
+      success: "",
+      btnText: "Login",
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
+  const signUpForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={name}
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            placeholder="Add your Full Name"></input>
+        </div>
+        <div className="form-group">
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="Add your email (exmple@domain.com)"></input>
+        </div>
+        <div className="form-group">
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="Setup your password"></input>
+        </div>
+        <div className="form-group">
+          <button className="btn btn-outline-warning">{btnText}</button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <h1>Sign Up</h1>
+        <br />
+        {signUpForm()}
+      </div>
+    </Layout>
+  );
 }
