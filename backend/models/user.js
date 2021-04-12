f14b7c25b0267a6ce2be7b9f52006f1a8630a0b0
slavo3dev <slavo@slavo3.com>
2021-04-12 15:07:11 -0400

    fixing formating code

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const crypto = require( "crypto" )
-
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,13 +10,13 @@ const userSchema = new mongoose.Schema(
       max: 12,
       unique: true,
       index: true,
-      lowercase: true,
+      lowercase: true
     },
     name: {
       type: String,
       trim: true,
       require: true,
-      max: 12,
+      max: 12
     },
     email: {
       type: String,
@@ -25,71 +24,65 @@ const userSchema = new mongoose.Schema(
       require: true,
       unique: true,
       max: 32,
-      lowercase: true,
+      lowercase: true
     },
     hashed_password: {
       type: String,
-      require: true,
+      require: true
     },
     salt: String,
     role: {
       type: String,
-      default: "subscriber",
+      default: "subscriber"
     },
     restetPasswordLink: {
       data: String,
-      default: "",
-    },
+      default: ""
+    }
   },
   { timestamps: true }
 );
 
 // virtual fileds
-userSchema.virtual( 'password' )
-  .set( function ( password ) {
+userSchema.virtual("password")
+  .set(function (password) {
     // create temp password
-    this._password = password
+    this._password = password;
 
     // generate slat
-    this.salt = this.makeSalt()
-    
-    //encrypt password 
-    this.hashed_password = this.encryptPassword(password)
+    this.salt = this.makeSalt();
 
-  } )
-  .get( function ()
-  {
-   return this._password
- })
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
 
-  authenticate: function (plainText)
-  {
-    return this.encryptPassword(plainText) === this.hashed_password
-  }, 
-
-  encryptPassword: function ( password ){
-    
-    if ( !password ) return ""
-    
-    try {
-      return crypto.createHmac( 'sha1', this.salt )
-        .update( passwords )
-        .digest("hex")
-    } catch ( err ) {
-      console.log( "Error: ", err )
-      return ""
-    }
-  
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
   },
-  
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+
+    try {
+      return crypto.createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      console.log("Error: ", err);
+      return "";
+    }
+  },
+
   // Make Salt
-  makeSlat: function ()
-  {
-     return Math.round(new Date().valueOf() * Math.random()) + ""
+  makeSlat: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
   }
 
-}
+};
 
-module.exports = mongoose.model("USER", userSchema)
+module.exports = mongoose.model("USER", userSchema);
