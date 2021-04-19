3e33ea808b01bf7890a3e04b1f07122ffcb1d582
slavo3dev <slavo@slavo3.com>
2021-04-19 09:53:07 -0400

    adding new modules

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || process.env.PORT;
 
 // Import Routes
 const authRoutes = require("./routes/auth");
@@ -13,6 +13,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
+
+
 
 // Middleware
 app.use("/api", authRoutes);
