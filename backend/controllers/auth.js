12fc817334405e9849f1b393eff8bed1e8347df1
slavo3dev <slavo@slavo3.com>
2021-05-28 10:59:56 -0400

    sending email

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -12,7 +12,6 @@ const ses = new AWS.SES({ appVersion: "2010-12-01" });
 
 exports.register = (req, res) => {
   const { name, email, password } = req.body;
-a
   // Check if User is in db
   USER.findOne({ email: email }).exec((_err, user) => {
     if (user) {
@@ -22,37 +21,45 @@ a
     }
     // generete user JWT token for auth
     const token = jwt.sign({ name, email, password }, process.env.JWT_ACCOUNT_ACTIVATION, { expiresIn: "10m" });
-  });
 
-  const params = {
-    Source: process.env.EMAIL_FROM,
-    Destination: {
-      ToAddresses: [email]
-    },
-    ReplyToAddresses: [process.env.EMAIL_TO],
-    Message: {
-      Body: {
-        Html: {
-          Charset: "UTF-8",
-          Data: `<html><body><h1>${name}</h1><p>!!!!You just hit end point!!!</p><p?>Your Password: ${password}</p?</body></html>`
-        }
+    // send email
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email]
       },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "Complete Your Registration"
+      ReplyToAddresses: [process.env.EMAIL_TO],
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `
+               <html>
+                 <body>
+                    <h1>Verify your email address</h1>
+                    <p>Please use the following link to complete your registration</p>
+                    <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+                  </body>
+                </html>`
+          }
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Complete Your Registration"
+        }
       }
-    }
-  };
+    };
 
-  const sendEmailOnRegistration = ses.sendEmail(params).promise();
+    const sendEmailOnRegistration = ses.sendEmail(params).promise();
 
-  sendEmailOnRegistration
-    .then(data => {
-      console.log(`Email submitet to SES: ${JSON.stringify(data)}`);
-      res.send("Email Sent");
-    })
-    .catch(err => {
-      console.log(`Error SES email:  Message: ${err}`);
-      res.send("Email Failed");
-    });
+    sendEmailOnRegistration
+      .then(data => {
+        console.log(`Email submitet to SES: ${JSON.stringify(data)}`);
+        res.send("Email Sent");
+      })
+      .catch(err => {
+        console.log(`Error SES email:  Message: ${err}`);
+        res.send("Email Failed");
+      });
+  });
 };
